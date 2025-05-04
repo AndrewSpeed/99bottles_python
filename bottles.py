6a5aa14587ea8a187f3de287fa500069ea3f8a96
Andrew Speed <2609244+AndrewSpeed@users.noreply.github.com>
2025-05-04 17:04:47 +0100

    Replace conditionals in BottleNumber with polymorphism, using BottleNumber0 and BottleNumber1 for specialisations

--- bottles.py
+++ bottles.py
@@ -1,3 +1,6 @@
+from typing import override
+
+
 class BottleNumber:
     _number: int
 
@@ -5,41 +8,69 @@ class BottleNumber:
         self._number = number
 
     def container(self) -> str:
-        if self._number == 1:
-            return "bottle"
         return "bottles"
 
     def pronoun(self) -> str:
-        if self._number == 1:
-            return "it"
         return "one"
 
     def successor(self) -> int:
-        if self._number == 0:
-            return 99
         return self._number - 1
 
     def quantity(self) -> str:
-        if self._number == 0:
-            return "no more"
         return str(self._number)
 
     def action(self) -> str:
-        if self._number == 0:
-            return "Go to the store and buy some more"
         return f"Take {self.pronoun()} down and pass it around"
+
+    @override
+    def __str__(self) -> str:
+        return f"{self.quantity()} {self.container()}"
+
+
+class BottleNumber0(BottleNumber):
+    @override
+    def quantity(self) -> str:
+        return "no more"
+
+    @override
+    def action(self) -> str:
+        return "Go to the store and buy some more"
+
+    @override
+    def successor(self) -> int:
+        return 99
+
+
+class BottleNumber1(BottleNumber):
+    @override
+    def container(self) -> str:
+        return "bottle"
+
+    @override
+    def pronoun(self) -> str:
+        return "it"
 
 
 class Bottles:
+    @staticmethod
+    def bottle_number_given(number: int) -> BottleNumber:
+        match number:
+            case 0:
+                return BottleNumber0(number)
+            case 1:
+                return BottleNumber1(number)
+            case _:
+                return BottleNumber(number)
+
     def verse(self, verse_number: int) -> str:
-        bottle_number = BottleNumber(verse_number)
-        next_bottle_number = BottleNumber(bottle_number.successor())
+        bottle_number = self.bottle_number_given(verse_number)
+        next_bottle_number = self.bottle_number_given(bottle_number.successor())
 
         return (
-            f"{bottle_number.quantity().capitalize()} {bottle_number.container()} of beer on the wall, "
-            f"{bottle_number.quantity()} {bottle_number.container()} of beer.\n"
-            f"{bottle_number.action()}, "
-            f"{next_bottle_number.quantity()} {next_bottle_number.container()} of beer on the wall.\n"
+            f"{bottle_number} of beer on the wall, ".capitalize()
+            + f"{bottle_number} of beer.\n"
+            + f"{bottle_number.action()}, "
+            + f"{next_bottle_number} of beer on the wall.\n"
         )
 
     def verses(self, verse_start: int, verse_end: int) -> str:
