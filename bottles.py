b2cb57dd9fcf97138ef9e3e3208b48adae354d7c
Andrew Speed <2609244+AndrewSpeed@users.noreply.github.com>
2025-05-04 13:55:02 +0100

    Now that generic verse and verse 2 are similar, extract difference into 'container' method

--- bottles.py
+++ bottles.py
@@ -1,4 +1,9 @@
 class Bottles:
+    def container(self, number: int) -> str:
+        if number == 1:
+            return "bottle"
+        return "bottles"
+
     def verse(self, verse_number: int) -> str:
         match verse_number:
             case 0:
@@ -15,19 +20,12 @@ class Bottles:
                     "Take it down and pass it around, "
                     "no more bottles of beer on the wall.\n"
                 )
-            case 2:
-                return (
-                    f"{verse_number} bottles of beer on the wall, "
-                    f"{verse_number} bottles of beer.\n"
-                    "Take one down and pass it around, "
-                    f"{verse_number - 1} bottle of beer on the wall.\n"
-                )
             case _:
                 return (
                     f"{verse_number} bottles of beer on the wall, "
                     f"{verse_number} bottles of beer.\n"
                     "Take one down and pass it around, "
-                    f"{verse_number - 1} bottles of beer on the wall.\n"
+                    f"{verse_number - 1} {self.container(verse_number - 1)} of beer on the wall.\n"
                 )
 
     def verses(self, verse_start: int, verse_end: int) -> str:
