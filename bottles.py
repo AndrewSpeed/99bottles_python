71cf100ce642075ca7f4ad0f12394c22cc367819
Andrew Speed <2609244+AndrewSpeed@users.noreply.github.com>
2025-05-04 14:13:50 +0100

    Replace remaining differences between verse 1 and generic case with pronoun and container methods

--- bottles.py
+++ bottles.py
@@ -9,6 +9,11 @@ class Bottles:
             return "it"
         return "one"
 
+    def quantity(self, number: int) -> str:
+        if number == 0:
+            return "no more"
+        return str(number)
+
     def verse(self, verse_number: int) -> str:
         match verse_number:
             case 0:
@@ -18,19 +23,12 @@ class Bottles:
                     "Go to the store and buy some more, "
                     "99 bottles of beer on the wall.\n"
                 )
-            case 1:
-                return (
-                    f"{verse_number} {self.container(verse_number)} of beer on the wall, "
-                    f"{verse_number} {self.container(verse_number)} of beer.\n"
-                    f"Take {self.pronoun(verse_number)} down and pass it around, "
-                    "no more bottles of beer on the wall.\n"
-                )
             case _:
                 return (
                     f"{verse_number} {self.container(verse_number)} of beer on the wall, "
                     f"{verse_number} {self.container(verse_number)} of beer.\n"
                     f"Take {self.pronoun(verse_number)} down and pass it around, "
-                    f"{verse_number - 1} {self.container(verse_number - 1)} of beer on the wall.\n"
+                    f"{self.quantity(verse_number - 1)} {self.container(verse_number - 1)} of beer on the wall.\n"
                 )
 
     def verses(self, verse_start: int, verse_end: int) -> str:
