fc0145018a598d867c08b0a9b19bd0468f83b7e1
Andrew Speed <2609244+AndrewSpeed@users.noreply.github.com>
2025-05-04 16:23:36 +0100

    Extract BottleNumber class

--- bottles.py
+++ bottles.py
@@ -1,35 +1,45 @@
-class Bottles:
-    def container(self, number: int) -> str:
-        if number == 1:
+class BottleNumber:
+    _number: int
+
+    def __init__(self, number: int):
+        self._number = number
+
+    def container(self) -> str:
+        if self._number == 1:
             return "bottle"
         return "bottles"
 
-    def pronoun(self, number: int) -> str:
-        if number == 1:
+    def pronoun(self) -> str:
+        if self._number == 1:
             return "it"
         return "one"
 
-    def successor(self, number: int) -> int:
-        if number == 0:
+    def successor(self) -> int:
+        if self._number == 0:
             return 99
-        return number - 1
+        return self._number - 1
 
-    def quantity(self, number: int) -> str:
-        if number == 0:
+    def quantity(self) -> str:
+        if self._number == 0:
             return "no more"
-        return str(number)
+        return str(self._number)
 
-    def action(self, number: int) -> str:
-        if number == 0:
+    def action(self) -> str:
+        if self._number == 0:
             return "Go to the store and buy some more"
-        return f"Take {self.pronoun(number)} down and pass it around"
+        return f"Take {self.pronoun()} down and pass it around"
 
+
+class Bottles:
     def verse(self, verse_number: int) -> str:
+        bottle_number = BottleNumber(verse_number)
+        next_bottle_number = BottleNumber(bottle_number.successor())
+
         return (
-            f"{self.quantity(verse_number).capitalize()} {self.container(verse_number)} of beer on the wall, "
-            f"{self.quantity(verse_number)} {self.container(verse_number)} of beer.\n"
-            f"{self.action(verse_number)}, "
-            f"{self.quantity(self.successor(verse_number))} {self.container(self.successor(verse_number))} of beer on the wall.\n"
+            f"{bottle_number.quantity().capitalize()} {bottle_number.container()} of beer on the wall, "
+            f"{bottle_number.quantity()} {bottle_number.container()} of beer.\n"
+            f"{bottle_number.action()}, "
+            f"{next_bottle_number.quantity()} {next_bottle_number.container()} of beer on the wall.\n"
         )
 
     def verses(self, verse_start: int, verse_end: int) -> str:
